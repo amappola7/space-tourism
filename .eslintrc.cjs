e2c7449e5438fddb6c7d602769c2019ea16e9d74
Amappola <anam.porrasp@gmail.com>
2022-11-17 20:47:42 -0500

    Setting up eslint, prettier and sass

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'prettier'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {}
+};
