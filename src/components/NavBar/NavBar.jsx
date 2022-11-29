a34293d44d13c1761233d9b13934778006a34707
Amappola <anam.porrasp@gmail.com>
2022-11-28 20:46:47 -0500

    Layout nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,1 +1,19 @@
+import React from 'react';
 import './NavBar.scss';
+
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <img src="src/assets/shared/logo.svg" alt="Logo" className="" />
+      <ul>
+        <li>HOME</li>
+        <li>DESTINATION</li>
+        <li>CREW</li>
+        <li>TECHNOLOGY</li>
+      </ul>
+      {/* BURGUER BUTTON */}
+    </nav>
+  );
+}
+
+export default NavBar;
